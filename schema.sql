f0c1850fc2fa6d539890d8d97c3efcb91c5f736d
michaelberg1005 <mh.berg@comcast.net>
2020-04-23 16:11:26 -0700

    adding challenge csvs and updated schema

--- schema.sql
+++ schema.sql
@@ -54,5 +54,213 @@ CREATE TABLE titles (
   FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
 );
 
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1955-12-31';
 
-SELECT * FROM departments;
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1952-12-31';
+
+-- Retirement eligibility
+SELECT first_name, last_name
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- Number of employees retiring
+SELECT COUNT(first_name)
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+
+SELECT first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+SELECT * FROM retirement_info;
+
+DROP TABLE retirement_info;
+
+-- Create new table for retiring employees
+SELECT emp_no, first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+-- Check the table
+SELECT * FROM retirement_info;
+
+-- Joining retirement_info and dept_emp tables
+SELECT ri.emp_no,
+	ri.first_name,
+ri.last_name,
+	de.to_date 
+FROM retirement_info as ri
+LEFT JOIN dept_emp as de
+ON ri.emp_no = de.emp_no;
+
+-- Joining departments and dept_manager tables 
+SELECT d.dept_name,
+     dm.emp_no,
+     dm.from_date,
+     dm.to_date
+FROM departments as d
+INNER JOIN dept_manager as dm
+ON d.dept_no = dm.dept_no;
+
+--joining retirement info and dept_emp for current employees
+SELECT ri.emp_no,
+	ri.first_name,
+	ri.last_name,
+de.to_date
+INTO current_emp
+FROM retirement_info as ri
+LEFT JOIN dept_emp as de
+ON ri.emp_no = de.emp_no
+WHERE de.to_date = ('9999-01-01');
+
+SELECT * FROM current_emp;
+
+-- Employee count by department number
+SELECT COUNT(ce.emp_no), de.dept_no
+INTO emp_by_dept
+FROM current_emp as ce
+LEFT JOIN dept_emp as de
+ON ce.emp_no = de.emp_no
+GROUP BY de.dept_no
+ORDER BY de.dept_no;
+
+SELECT * FROM emp_by_dept;
+
+SELECT * FROM salaries
+ORDER BY to_date DESC;
+
+--Creat temp employee info table for salary join
+SELECT emp_no,
+	first_name,
+last_name,
+	gender
+INTO emp_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+DROP TABLE emp_info CASCADE;
+
+SELECT e.emp_no,
+	e.first_name,
+e.last_name,
+	e.gender,
+	s.salary,
+	de.to_date
+INTO emp_info
+FROM employees as e
+INNER JOIN salaries as s
+ON (e.emp_no = s.emp_no)
+INNER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+     AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+	  AND (de.to_date = '9999-01-01');
+	  
+SELECT * FROM emp_info;
+
+-- List of managers per department
+SELECT  dm.dept_no,
+        d.dept_name,
+        dm.emp_no,
+        ce.last_name,
+        ce.first_name,
+        dm.from_date,
+        dm.to_date
+--INTO manager_info
+FROM dept_manager AS dm
+    INNER JOIN departments AS d
+        ON (dm.dept_no = d.dept_no)
+    INNER JOIN current_emp AS ce
+        ON (dm.emp_no = ce.emp_no);
+
+--department info list
+SELECT ce.emp_no,
+ce.first_name,
+ce.last_name,
+d.dept_name	
+-- INTO dept_info
+FROM current_emp as ce
+INNER JOIN dept_emp AS de
+ON (ce.emp_no = de.emp_no)
+INNER JOIN departments AS d
+ON (de.dept_no = d.dept_no);
+
+--Create table for retirement by title - table 1 of challenge
+SELECT ri.emp_no,
+ ri.first_name,
+ ri.last_name,
+ titles.title,
+ titles.from_date,
+ salaries.salary
+INTO retirement_by_title
+FROM retirement_info AS ri
+INNER JOIN titles
+ON (ri.emp_no = titles.emp_no)
+INNER JOIN salaries
+ON (ri.emp_no = salaries.emp_no);
+
+SELECT * FROM retirement_by_title;
+
+DROP TABLE retirement_data CASCADE;
+
+-- Partition the data to show only most recent title per employee
+SELECT emp_no, 
+	first_name,
+	last_name,
+	from_date,
+	title,
+	salary
+INTO retirement_data
+FROM
+  (SELECT emp_no, 
+	first_name,
+	last_name,
+	from_date,
+	title, salary,
+   ROW_NUMBER() OVER 
+	(PARTITION BY (emp_no) 
+	 ORDER BY from_date DESC) rn
+   FROM retirement_by_title
+  ) tmp WHERE rn = 1
+  ORDER BY emp_no;
+
+SELECT Count(emp_no),title
+INTO retirement_title
+FROM retirement_data
+GROUP BY title
+ORDER BY title;
+
+SELECT * FROM retirement_title;
+
+--Creating initial table for mentorship - CHALLENGE TABLE 2
+
+SELECT emp_no, first_name, last_name
+INTO mentorship
+FROM employees
+WHERE (birth_date BETWEEN '1965-01-01' AND '1965-12-31');
+
+SELECT * FROM mentorship;
+
+--Join mentorship list with titles and dates and filter by current employees
+SELECT mentorship.emp_no,
+mentorship.first_name,
+mentorship.last_name,
+titles.title,
+titles.from_date,
+titles.to_date
+INTO mentorship_list
+FROM mentorship
+LEFT JOIN titles
+ON mentorship.emp_no = titles.emp_no
+WHERE titles.to_date = ('9999-01-01');
